334f77ebcdd6662067945a2ce881a96e7b3ab4be
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2016-10-08 21:30:21 -0500

    /Users/larry/tmp/COMMIT_EDITMSG

--- project.clj
+++ project.clj
@@ -1,6 +1,13 @@
 (defproject cljs-utils "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Useful utility functions."
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :license {:name "Apache 2.0 License"
+            :url "https://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [[org.clojure/clojure "1.8.0"]i
+                 [org.clojure/clojurescript "1.8.51" :classifier "aot"]]
+  :jvm-opts ^:replace ["-Xmx1g" "-server"]
+  :plugins [[lein-npm "0.6.2"]]
+  :npm {:dependencies [[source-map-support "0.4.2"]]}
+  :source-paths ["src" "target/classes"]
+  :clean-targets ["out" "release"]
+  :target-path "target")
